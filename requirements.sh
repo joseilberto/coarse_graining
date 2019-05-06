1f97aa2d62d8ae9cdbf1b1da692cb463b11168bf
José Ilberto <ilbertofjunior@gmail.com>
2019-05-06 12:56:25 +0200

    [load] Added dev headers to requirements

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,6 @@
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
-sudo apt-get install python3.6 python3.6-venv -y
+sudo apt-get install python3.6 python3.6-venv python3.6-dev -y
 python3.6 -m pip install
 pip install virtualenv
 python3 -m venv ./venv

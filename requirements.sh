1ac2533918a1e789617de4218d0631bac0955f27
José Ilberto <ilbertofjunior@gmail.com>
2019-05-10 13:57:29 +0200

    Changed project to anaconda to support tensorflow 1.13.1

--- requirements.sh
+++ requirements.sh
@@ -1,8 +1,4 @@
-sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt-get update
-sudo apt-get install python3.6 python3.6-venv python3.6-dev -y
-python3.6 -m pip install
-pip install virtualenv
-python3 -m venv ./venv
-source ./venv/bin/activate
-pip install -r requirements_python.txt
+conda create -n coarse_graining python=3.7.3 \
+    ipython ipdb matplotlib opencv pandas tensorflow-gpu \
+    -c conda-forge -y
+

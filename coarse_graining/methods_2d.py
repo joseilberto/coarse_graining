4a11b4453683888f59c79240cda474bc05d63f6c
José <ilbertofjunior@gmail.com>
2019-05-12 15:57:47 +0200

    Implemented coarse graining for momenta

--- coarse_graining/methods_2d.py
+++ coarse_graining/methods_2d.py
@@ -9,21 +9,6 @@ class Coarse_Graining(Coarse_Base):
         Y = tf.reshape(positions[:, 1], [-1, 1, 1])
         return tf.sqrt((grid_centers[0] - X)**2 + (grid_centers[1] - Y)**2)
 
-
-    def density_grid_updater(self, X, Y, *args, **kwargs):
-        self.idxs = self.find_indexes(self.pos, [self.xx, self.yy])        
-        self.regions = self.find_regions(X, Y)
-        masses = self.find_sphere_masses()
-        self.density_updates = self.densities_updater(self.regions, masses)
-
-
-    def densities_updater(self, regions, masses, *args, **kwargs):
-        self.volume_fraction = tf.exp(-regions**2 / (2 * self.W**2))
-        total_volume = tf.reduce_sum(self.volume_fraction, axis = [1, 2])
-        mass_density = masses*(1/self.cell_size**2)
-        scaled_mass = tf.reshape(mass_density / total_volume, [-1, 1, 1])        
-        return scaled_mass * self.volume_fraction
-
     
     def extend_limits(self, limits, *args, **kwargs):
         min_x = limits[0] - 2*self.n_points*self.cell_size
@@ -31,6 +16,49 @@ class Coarse_Graining(Coarse_Base):
         min_y = limits[2] - 2*self.n_points*self.cell_size
         max_y = limits[3] + 2*self.n_points*self.cell_size
         return [min_x, max_x, min_y, max_y]
+
+
+    def fill_density_grid(self, X, Y, radii, *args, **kwargs):
+        if not hasattr(self, "session"):
+            self.session = tf.InteractiveSession()
+        if not hasattr(self, "idxs"):
+            self.idxs = self.find_indexes(self.pos, [self.xx, self.yy])
+        if not hasattr(self, "regions"):           
+            self.regions = self.find_regions(X, Y)
+        masses = self.find_sphere_masses()
+        density_updates = self.updater_densities(self.regions, masses)
+        init = tf.global_variables_initializer()
+        self.session.run(init)        
+        idxs, density_updates = self.session.run((self.idxs, density_updates),
+                        feed_dict = {
+                            self.pos: np.column_stack((X, Y)),
+                            self.radii: radii,
+                        })   
+        self.session.close()
+        del self.session
+        self.densities = self.update_grid(self.densities, idxs, density_updates)        
+
+
+    def fill_momenta_grid(self, X, Y, V_X, V_Y, radii, *args, **kwargs):
+        if not hasattr(self, "session"):
+            self.session = tf.InteractiveSession()
+        if not hasattr(self, "idxs"):
+            self.idxs = self.find_indexes(self.pos, [self.xx, self.yy])
+        if not hasattr(self, "regions"):           
+            self.regions = self.find_regions(X, Y)
+        masses = self.find_sphere_masses()
+        momenta_updates = self.updater_momenta(self.regions, masses)
+        init = tf.global_variables_initializer()
+        self.session.run(init)
+        idxs, momenta_updates = self.session.run((self.idxs, momenta_updates),
+                        feed_dict = {
+                            self.pos: np.column_stack((X, Y)),
+                            self.vels: np.column_stack((V_X, V_Y)),
+                            self.radii: radii,
+                        })
+        self.session.close()
+        del self.session
+        self.momenta = self.update_grid(self.momenta, idxs, momenta_updates)
 
 
     def find_indexes(self, positions, grid_centers, *args, **kwargs):
@@ -67,35 +95,19 @@ class Coarse_Graining(Coarse_Base):
 
     def find_sphere_masses(self, *args, **kwargs):
         return 4*np.pi*self.radii
-
+        
 
     def kinetic_stress(self, X, Y, V_X, V_Y, radii, *args, **kwargs):
         self.make_updates(X, Y, V_X, V_Y, radii, *args, **kwargs)
 
 
     def make_updates(self, X, Y, V_X, V_Y, radii, *args, **kwargs):
-        self.session = tf.InteractiveSession()
-        self.start_tf_variables()
-        self.start_grids(X, Y, *args, **kwargs)
-        self.density_grid_updater(X, Y)
-        init = tf.global_variables_initializer()
-        self.session.run(init)
-        idxs, density_updates = self.session.run((self.idxs, self.density_updates),
-                        feed_dict = {
-                            self.pos: np.column_stack((X, Y)),
-                            self.radii: radii,
-                        })   
-        self.densities = self.update_grid(self.densities, idxs, density_updates)
-        self.session.close()
+        self.start_grids_and_variables(X, Y, *args, **kwargs)        
+        self.fill_density_grid(X, Y, radii)
+        self.fill_momenta_grid(X, Y, V_X, V_Y, radii)              
 
-
-    def start_tf_variables(self, *args, **kwargs):
-        self.pos = tf.placeholder(tf.float32, shape = (None, 2))
-        self.vels = tf.placeholder(tf.float32, shape = (None, 2))
-        self.radii = tf.placeholder(tf.float32, shape = (None, ))
-
-
-    def start_grids(self, X, Y, *args, **kwargs):
+ 
+    def start_grids_and_variables(self, X, Y, *args, **kwargs):
         limits = getattr(self, "limits", None)        
         if not limits:
             limits = [X.min(), X.max(), Y.min(), Y.max()]
@@ -104,9 +116,12 @@ class Coarse_Graining(Coarse_Base):
         self.ys = np.arange(*limits[2:], self.cell_size)
         self.xx, self.yy = np.meshgrid(self.xs, self.ys)
         self.densities = np.zeros(self.xx.shape)
-        self.momentum = np.zeros(self.xx.shape + (2,))
+        self.momenta = np.zeros(self.xx.shape + (2,))
         self.kinetic = np.zeros(self.xx.shape + (4,))
         self.kinect_trace = np.zeros(self.xx.shape)
+        self.pos = tf.placeholder(tf.float32, shape = (None, 2))
+        self.vels = tf.placeholder(tf.float32, shape = (None, 2))
+        self.radii = tf.placeholder(tf.float32, shape = (None, ))
 
 
     def update_grid(self, grid, idxs, updates, *args, **kwargs):
@@ -115,3 +130,23 @@ class Coarse_Graining(Coarse_Base):
             min_y, max_y = region[:, 1]
             grid[min_y:max_y, min_x:max_x] += updates[idx]            
         return grid
+
+
+    def updater_densities(self, regions, masses, *args, **kwargs):
+        if not hasattr(self, "volume_fraction"):
+            self.volume_fraction = tf.exp(-regions**2 / (2 * self.W**2))
+        total_volume = tf.reduce_sum(self.volume_fraction, axis = [1, 2])
+        mass_density = masses*(1/self.cell_size**2)
+        scaled_mass = tf.reshape(mass_density / total_volume, [-1, 1, 1])        
+        return scaled_mass * self.volume_fraction
+    
+
+    def updater_momenta(self, regions, masses, *args, **kwargs):
+        if not hasattr(self, "volume_fraction"):
+            self.volume_fraction = tf.exp(-regions**2 / (2 * self.W**2))
+        total_volume = tf.reduce_sum(self.volume_fraction, axis = [1, 2])
+        scaled_momentum_x = tf.reshape(self.vels[:, 0]*masses/total_volume, 
+                                        [-1, 1, 1])*self.volume_fraction
+        scaled_momentum_y = tf.reshape(self.vels[:, 1]*masses/total_volume, 
+                                        [-1, 1, 1])*self.volume_fraction
+        return tf.stack([scaled_momentum_x, scaled_momentum_y], axis = 3)
